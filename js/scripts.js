8099329ecf9604b933b6ef864be17e1ac143fa39
interruta <interruta.sistincon@gmail.com>
2024-01-17 19:32:55 -0500

    Ninja game Javascript canva

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+
+
+// Agregar un controlador de eventos de clic al elemento
+const airplaneLink = document.getElementById('ninja-link');
+airplaneLink.addEventListener('click', function () {
+    // Redirigir a airplane.html cuando se hace clic en el elemento
+    window.location.href = 'ninja.html';
+});
+
+// Agregar un controlador de eventos de clic al elemento
+const simioLink = document.getElementById('simio-link');
+simioLink.addEventListener('click', function () {
+    // Redirigir a simio.html cuando se hace clic en el elemento
+    window.location.href = 'simio.html';
+});
